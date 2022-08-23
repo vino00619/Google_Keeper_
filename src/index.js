aa04f4839ae4044c5af80f70b0f1894e7204fa5d
vino00619 <vinoth00619@gmail.com>
2022-08-23 20:47:19 +0530

    z

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App";
-import Header from "./Components/Header.jsx";
-import Footer from "./Components/Footer.jsx";
+// import Header from "./Components/Header.jsx";
+// import Footer from "./Components/Footer.jsx";
 
 //comment
 
